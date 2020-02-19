b4138971ac249d8b150139af71654ece0d00b9ec
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2020-02-19 12:41:28 +0100

    Added tile-rectangle.js and rectangle-chain.js extensions
    Two custom tools, one working with tile layers and one inserting objects.
    Both of them featuring a nice scalable icon.
    
    I've written tile-rectangle.js while live-streaming on Twitch. The video
    is available at https://www.youtube.com/watch?v=NJsC5Kedumo.

---
+++ rectangle-chain.js
@@ -0,0 +1,55 @@
+/*
+ * rectangle-chain.js
+ *
+ * Example tool that places rectangle objects when the mouse is dragged over
+ * the map.
+ */
+
+var tool = tiled.registerTool('RectangleChain', {
+    name: "Draw Rectangle Chain",
+    icon: "rectangle-chain.svg",
+
+    mouseMoved(x, y, modifiers) {
+        if (!this.pressed)
+            return
+
+        var dx = Math.abs(this.x - x)
+        var dy = Math.abs(this.y - y)
+
+        this.distance += Math.sqrt(dx*dx + dy*dy)
+        this.x = x
+        this.y = y
+
+        if (this.distance > 32) {
+            var objectLayer = this.map.currentLayer
+
+            if (objectLayer && objectLayer.isObjectLayer) {
+                var object = new MapObject(MapObject.Rectangle, ++this.counter)
+                object.x = Math.min(this.lastX, x)
+                object.y = Math.min(this.lastY, y)
+                object.width = Math.abs(this.lastX - x)
+                object.height = Math.abs(this.lastY - y)
+                objectLayer.addObject(object)
+                object.selected = true
+            }
+
+            this.distance = 0
+            this.lastX = x
+            this.lastY = y
+        }
+    },
+
+    mousePressed(button, x, y, modifiers) {
+        this.pressed = true
+        this.x = x
+        this.y = y
+        this.distance = 0
+        this.counter = 0
+        this.lastX = x
+        this.lastY = y
+    },
+
+    mouseReleased(button, x, y, modifiers) {
+        this.pressed = false
+    },
+})
