3a4af413509b17cbe31a9ed9d1ec37be810c72af
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2020-02-19 11:47:37 +0100

    Added follow-warp.js extension
    Basic example of a useful action.
    
    I've written this script while live-streaming on Twitch and the one-hour
    video is available at https://www.youtube.com/watch?v=ip8e1jkEZWE.

---
+++ follow-warp.js
@@ -0,0 +1,75 @@
+/*
+ * follow-warp.js
+ *
+ * This extension adds a 'Follow Selected Warp' (Ctrl+F) action to the Map
+ * menu, which can be used to jump to the destination of a selected warp object
+ * (as used on maps made for Source of Tales - http://sourceoftales.org/).
+ *
+ * Warps are common in games and there are of course many ways to implement
+ * them, so this script is unlikely to work in your particular project. But
+ * with some adjustments it probably could!
+ */
+
+function findObjectByName(thing, name) {
+    for (let i = thing.layerCount - 1; i >= 0; i--) {
+        const layer = thing.layerAt(i)
+
+        if (layer.isGroupLayer) {
+            const obj = findObjectByName(layer, name)
+            if (obj)
+                return obj
+        } else if (layer.isObjectLayer) {
+            for (const obj of layer.objects)
+                if (obj.name == name)
+                    return obj
+        }
+    }
+
+    return null
+}
+
+let followWarp = tiled.registerAction('FollowWarp', function(action) {
+    const map = tiled.activeAsset
+    if (!map.isTileMap) {
+        tiled.alert("Not a tile map!")
+        return
+    }
+
+    const selectedObject = map.selectedObjects[0]
+    if (!selectedObject) {
+        tiled.alert("No object selected!")
+        return
+    }
+
+    const destMapProperty = selectedObject.property("DEST_MAP")
+    if (!destMapProperty) {
+        tiled.alert("No DEST_MAP property!")
+        return
+    }
+
+    const mapsPath = map.fileName.substr(0, map.fileName.indexOf("maps/") + 5)
+    const destinationMapFile = mapsPath + destMapProperty + ".tmx"
+    const destinationName = selectedObject.property("DEST_NAME")
+
+    const destinationMap = tiled.open(destinationMapFile)
+    if (!destinationMap)
+        return
+
+    if (destinationName) {
+        const object = findObjectByName(destinationMap, destinationName)
+        if (!object) {
+            tiled.alert("Failed to find object named '" + destinationName + "'")
+            return
+        }
+
+        tiled.mapEditor.currentMapView.centerOn(object.x, object.y)
+        destinationMap.selectedObjects = [object]
+    }
+})
+followWarp.text = "Follow Selected Warp"
+followWarp.shortcut = "Ctrl+F"
+
+tiled.extendMenu("Map", [
+    { separator: true },
+    { action: "FollowWarp" },
+])
